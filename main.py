282b469c46f9bee9494c9135f4ae254888b498fd
NikitasThermos <nikiras.t@gmail.com>
2023-12-01 13:07:58 +0200

    Added basic dense network

--- main.py
+++ main.py
@@ -4,7 +4,7 @@ import pandas as pd
 import numpy as np
 
 from preprocessing import show_dataset_stats, preprocess_dataset
-from models import sgd, random_forest
+from models import sgd, random_forest, dense_network
 
 
 def load_csv():
@@ -16,7 +16,8 @@ def main():
     show_dataset_stats(df)
     X_train, X_val, y_train, y_val = preprocess_dataset(df)
     sdg_pred = sgd(X_train, y_train, X_val) 
-    rand_forest = random_forest(X_train, y_train, X_val)
+    forest_pred = random_forest(X_train, y_train, X_val)
+    net_pred = dense_network(X_train, y_train, X_val, y_val)
     
 if __name__ == '__main__': 
     main()
--- models.py
+++ models.py
@@ -2,6 +2,9 @@ from sklearn.linear_model import SGDClassifier
 from sklearn.ensemble import RandomForestClassifier
 from sklearn.model_selection import GridSearchCV
 
+import tensorflow as tf
+tf.random.set_seed(42)
+
 def sgd(X_train, y_train, X_val):
     sgd_clf = SGDClassifier(random_state=42)
     param_grid = {'alpha':[0.0003, 0.0005, 0.0007], 'tol':[1e-4, 1e-3, 1e-2],
@@ -26,3 +29,31 @@ def random_forest(X_train, y_train, X_val):
     print('Random Forest best parameters:')
     print(grid_search.best_params_)
     return grid_search.predict(X_val)
+
+def dense_network(X_train, y_train, X_val, y_val):
+    train_dataset = tf.data.Dataset.from_tensor_slices((X_train, y_train))
+    val_dataset = tf.data.Dataset.from_tensor_slices((X_val, y_val))
+
+    train_dataset = train_dataset.shuffle(buffer_size=3000).batch(32)
+    val_dataset = val_dataset.batch(32)
+
+    model = tf.keras.Sequential([
+        tf.keras.layers.Dense(64, activation='relu', input_shape=(64,)),
+        tf.keras.layers.Dense(1, activation='sigmoid')
+    ])
+
+    early_stopping_cb = tf.keras.callbacks.EarlyStopping(patience=5,
+                                                         restore_best_weights=True)
+    callabacks = [early_stopping_cb]
+
+    optimizer = tf.keras.optimizers.Adam(learning_rate=0.001,
+                                         beta_1=0.9, beta_2=0.999)
+    model.compile(loss="binary_crossentropy",
+                  optimizer=optimizer,
+                  metrics=["accuracy"])
+    
+    history = model.fit(train_dataset, epochs=30,
+                        validation_data=val_dataset,
+                        callabacks=callabacks)
+
+    return model.predict(X_val)
