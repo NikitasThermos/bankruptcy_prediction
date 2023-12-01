8a3b19d941b38c1b9806a0f98e4e2d48d6515e34
NikitasThermos <nikiras.t@gmail.com>
2023-12-01 13:39:24 +0200

    Logging predictions

--- main.py
+++ main.py
@@ -2,6 +2,7 @@ import os
 os.environ['TF_CPP_MIN_LOG_LEVEL'] = '1' 
 import pandas as pd 
 import numpy as np
+from sklearn.metrics import confusion_matrix
 
 from preprocessing import show_dataset_stats, preprocess_dataset
 from models import sgd, random_forest, dense_network
@@ -10,14 +11,21 @@ from models import sgd, random_forest, dense_network
 def load_csv():
     return pd.read_csv('companydata.csv')
 
+def log_results(y_val, **predictions):
+    print('Logging results for the evaluation dataset')
+    for model, pred in predictions.items(): 
+        print(f'Confusion Matrix for {model} :')
+        print(confusion_matrix(y_val, pred))
 
 def main(): 
     df = load_csv() 
     show_dataset_stats(df)
     X_train, X_val, y_train, y_val = preprocess_dataset(df)
-    sdg_pred = sgd(X_train, y_train, X_val) 
-    forest_pred = random_forest(X_train, y_train, X_val)
-    net_pred = dense_network(X_train, y_train, X_val, y_val)
+    predictions = dict()
+    predictions['SGD'] = sgd(X_train, y_train, X_val) 
+    predictions['Random Forest'] = random_forest(X_train, y_train, X_val)
+    predictions['Dense Network'] = dense_network(X_train, y_train, X_val, y_val)
+    log_results(**predictions)
     
 if __name__ == '__main__': 
     main()
