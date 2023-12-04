d11a87467f8564bab9b4f29d15d55254092330e1
NikitasThermos <nikiras.t@gmail.com>
2023-12-04 16:59:30 +0200

    Added argument for model selection

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
 import os
-os.environ['TF_CPP_MIN_LOG_LEVEL'] = '1' 
+os.environ['TF_CPP_MIN_LOG_LEVEL'] = '1'
+import sys
+import argparse
+
 import pandas as pd 
 import numpy as np
 from sklearn.metrics import confusion_matrix, recall_score, precision_score, f1_score
@@ -8,6 +11,15 @@ from tabulate import tabulate
 from preprocessing import show_dataset_stats, preprocess_dataset
 from models import sgd, random_forest, dense_network
 
+def parse_arguments(sys_argv): 
+    parser = argparse.ArgumentParser() 
+
+    parser.add_argument('--model',
+                        help='Select the model to use',
+                        choices=['all','SGD', 'RF', 'DNN'],
+                        default='all',
+                        type=str)
+    return parser.parse_args(sys_argv)
 
 def load_csv():
     return pd.read_csv('companydata.csv')
@@ -25,13 +37,25 @@ def log_results(y_val, **predictions):
     print(table)
 
 def main(): 
+    args = parse_arguments(sys.argv[1:])
     df = load_csv() 
     show_dataset_stats(df)
     X_train, X_val, y_train, y_val = preprocess_dataset(df)
     predictions = dict()
-    predictions['SGD'] = sgd(X_train, y_train, X_val) 
-    predictions['Random Forest'] = random_forest(X_train, y_train, X_val)
-    predictions['Dense Network'] = dense_network(X_train, y_train, X_val, y_val)
+
+    match args.model:
+        case 'SGD':
+            predictions['SGD'] = sgd(X_train, y_train, X_val) 
+        case 'RF':
+            predictions['Random Forest'] = random_forest(X_train, y_train, X_val)
+        case 'DNN':
+            predictions['Dense Network'] = dense_network(X_train, y_train, X_val, y_val)
+        case 'all':        
+            predictions['SGD'] = sgd(X_train, y_train, X_val) 
+            predictions['Random Forest'] = random_forest(X_train, y_train, X_val)
+            predictions['Dense Network'] = dense_network(X_train, y_train, X_val, y_val)
+        case _:
+            raise Exception(f'model:{args.model} not found')
     log_results(**predictions)
     
 if __name__ == '__main__': 
