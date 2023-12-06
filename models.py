2fb7bf399403a620381df1290ab0186ea40b8dd9
NikitasThermos <nikiras.t@gmail.com>
2023-12-06 13:30:58 +0200

    Changed grid search from accuracy to f1 for SGD and Random Forest

--- models.py
+++ models.py
@@ -11,7 +11,7 @@ def sgd(X_train, y_train, X_val):
     param_grid = {'alpha':[0.0003, 0.0005, 0.0007], 'tol':[1e-4, 1e-3, 1e-2],
                   'epsilon': [0.05, 0.1, 0.2]}
     grid_search = GridSearchCV(sgd_clf, param_grid, cv=5, 
-                               scoring='accuracy',
+                               scoring='f1',
                                return_train_score=True)
     grid_search.fit(X_train, y_train)
 
@@ -25,7 +25,7 @@ def random_forest(X_train, y_train, X_val):
     forest_clf = RandomForestClassifier(random_state=42, criterion='entropy')
     param_grid = {'n_estimators':[4, 5, 6], 'min_samples_split':[6, 8, 10, 12]}
     grid_search = GridSearchCV(forest_clf, param_grid, 
-                                cv=3, scoring='accuracy',
+                                cv=3, scoring='f1',
                                 return_train_score=True, verbose=True)
     grid_search.fit(X_train, y_train)
     print('Random Forest best parameters:')
